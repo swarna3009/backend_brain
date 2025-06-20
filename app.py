a34ce076cafea531d20fd5f96fd8bed1ce42b6c1
swarna3009 <145216193+swarna3009@users.noreply.github.com>
2025-06-20 21:44:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from bson import json_util
 import random
 
 app = Flask(__name__)
-CORS(app)
+CORS(app,origins=["https://brain-frontend3.vercel.app/"])
 
 # Secret key for JWT encoding/decoding
 app.config['SECRET_KEY'] = 'your_secret_key_here'
